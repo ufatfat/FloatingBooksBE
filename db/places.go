6652b4f7ae2f7d041487b501eded3e1e77e6f606
ufatfat <lzff@ufatfat.com>
2021-04-18 22:05:14 +0800

    修改了一个拼写错误

--- db/places.go
+++ db/places.go
@@ -3,6 +3,6 @@ package db
 import "FloatingBooks/model"
 
 func GetPlaces () (places []model.PlaceInfo, err error) {
-	err = Mysql.Table("locations").Select("id", "location_name").Order("localtion_abbr").Scan(&places).Error
+	err = Mysql.Table("locations").Select("id", "location_name").Order("location_abbr").Scan(&places).Error
 	return
 }
